5c1b5e4b36e47650f8c3be256c6259210f163c94
Irfan1983 <34341287+Irfan1983@users.noreply.github.com>
2021-07-22 18:34:09 +0400

    tesst

---
+++ Facebook/src/testcase/TC1.java
@@ -0,0 +1,21 @@
+package testcase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC1 {
+	
+	WebDriver driver;
+	
+	@Test 
+	public void TC111()
+	{
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		System.out.println("test");
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.xpath("//*[@id=\"pageFooter\"]/ul/li[2]/a")).click();
+	}
+
+}
